90dda076cf08ff2af4640250def4e554ad7f2616
Baptiste THIVEND <baptiste.thivend@insa-lyon.fr>
2021-03-08 17:34:04 +0100

    Update test_settings.py

--- matomo_import/settings.py
+++ matomo_import/settings.py
@@ -1,22 +1,35 @@
 import urllib3
-import sqlite3
 import yaml
-import sys
-from os import remove
 
 
 def init(settings_file='secrets.yml'):
     set_http_manager()
-    set_database(settings_file)
-
-
-def set_database(settings_file):
-    global connection, secrets
-    with open(settings_file, 'r') as f:
-        secrets = yaml.safe_load(f)
-        connection = sqlite3.connect(secrets["api_settings"]["db_path"])
+    set_secrets(settings_file)
+    set_database()
 
 
 def set_http_manager():
     global http
     http = urllib3.PoolManager()
+
+
+def set_secrets(settings_file):
+    global secrets
+    with open(settings_file, 'r') as f:
+        secrets = yaml.safe_load(f)
+
+
+def set_database():
+    assert secrets
+    global connection
+    connection = specify_database_provider(secrets['db_settings'])
+
+
+def specify_database_provider(
+        db_settings={'db_provider': 'sqlite3', 'db_name': 'default'}
+        ):
+    if db_settings['db_provider'] == 'sqlite3':
+        import sqlite3
+        return sqlite3.connect(db_settings['db_name'])
+    else:
+        raise NotImplementedError("Database provider not handled")
