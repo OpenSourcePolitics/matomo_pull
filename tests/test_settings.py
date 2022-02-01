69ae1908243f9423cb2cda5ae28c1f4c3edd9253
THIVEND <baptiste.thivend@protonmail.com>
2022-02-01 10:25:50 +0100

    Correct test_settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,8 +1,7 @@
 import pytest
-import os
 import matomo_pull.settings as settings
 import matomo_pull.utils as utils
-from tests.utils import (
+from .conftest import (
     rdv_for_tests,
     settings_setup,
     settings_init,
@@ -43,7 +42,7 @@ def test_database_variables_wrongly_set(tmpdir, monkeypatch):
 
 
 def test_database_already_up_to_date(settings_setup, monkeypatch):
-    from datetime import date, timedelta, datetime
+    from datetime import date, timedelta
     monkeypatch.setattr(
         settings,
         "update_start_date_regarding_database_state",
@@ -64,13 +63,17 @@ def test_database_creation(settings_setup):
 
 
 def test_updating_dates(settings_init, settings_setup):
-    from datetime import datetime, date, timedelta
+    from datetime import datetime, timedelta
     from random import randint
-    last_update_date = datetime.now()  - timedelta(days=randint(1, 100))
+    last_update_date = datetime.now() - timedelta(days=randint(1, 100))
     conn = settings.set_database_connection()
 
-    conn.execute("create table visits(id int primary key not null, date timestamp);")
-    conn.execute(f"insert into visits(id,date) values(1, '{last_update_date}')")
+    conn.execute(
+        "create table visits(id int primary key not null, date timestamp);"
+    )
+    conn.execute(
+        f"insert into visits(id,date) values(1, '{last_update_date}')"
+    )
 
     vars = settings.check_mtm_vars(settings.mtm_vars)
     assert vars['start_date'] == last_update_date.date() + timedelta(days=1)
